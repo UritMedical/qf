b27fd7f6ca0937697a9a9cc6d5abcac7431cba0f
廖志斌 <kamioair@qq.com>
2023-03-14 08:29:25 +0800

    补充提交

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/UritMedical/qf"
 	"github.com/UritMedical/qf/mc/patient"
+	"github.com/UritMedical/qf/mc/user"
 )
 
 func main() {
@@ -12,7 +13,7 @@ func main() {
 func regBll(s *qf.Service) {
 	// 注册框架提供的通用业务
 	// 通用业务位于mc文件夹内
-	//s.RegBll(&user.Bll{}, "")    // 用户业务
+	s.RegBll(&user.Bll{}, "")    // 用户业务
 	s.RegBll(&patient.Bll{}, "") // 患者信息业务
 
 	// 注册自定义业务
